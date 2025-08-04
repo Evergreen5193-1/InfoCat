919c1b3d2b5d6652664cd5af8aaacd7b37f8ef0a
Evergreen5193-1 <evergreenrok5193@gmail.com>
2025-08-04 12:44:25 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 // auth.js
 
 // IMPORTANT: These are public keys, it is safe to expose them in your frontend code.
-const SUPABASE_URL = 'YOUR_SUPABASE_PROJECT_URL';
+const SUPABASE_URL = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBvemtwZm55c2Nibm1hZmtncXl6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI5NzUyNzgsImV4cCI6MjA2ODU1MTI3OH0.EsEIdX25oH-gTb14XLTm3eYfsb8xEueLDn6ACPPMkeg';
 const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_PUBLIC_KEY';
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
